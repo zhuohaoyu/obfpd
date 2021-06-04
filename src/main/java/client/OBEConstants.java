49c321388b66e88154cc461c29fdbab5b041c829
zhuohaoyu <zhuohaoyu1228@outlook.com>
2021-06-04 10:35:02 +0800

    add gradle files

---
+++ src/main/java/client/OBEConstants.java
@@ -0,0 +1,6 @@
+package main.java.client;
+
+public interface OBEConstants {
+    String OBE_LOGIN_URL = "http://obe.ruc.edu.cn/index/login/login.html";
+    String OBE_USER_COURSEWORK_URL = "http://obe.ruc.edu.cn/index/home/morenews.html";
+}
