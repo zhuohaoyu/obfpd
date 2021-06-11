02a9c14966946b70a81575f9b7791c6433a82bd1
izumihanako <wangyuansen@outlook.com>
2021-06-11 12:25:12 +0800

    修改了首页作业剩余时间的计算逻辑
    Signed-off-by: izumihanako <wangyuansen@outlook.com>

--- src/main/java/ui/HomePanel.java
+++ src/main/java/ui/HomePanel.java
@@ -193,10 +193,14 @@ public class HomePanel extends Panel {
         if( day > 243 ) day = -day + 243 ; // 9.1为基准
         else if( day > 90 ) day = -day + 90 ;
         int fix = App.student.getDayLimitHomeworkCnt( day ) ;
-        homeworktot.setText( "待完成的作业：" + Integer.toString( App.student.getDayLimitHomeworkCnt(999) - fix ) + " 项" ) ;
-        homework3d.setText( "剩余时间不足3天的作业：" + Integer.toString( App.student.getDayLimitHomeworkCnt(3) - fix ) + " 项" ) ;
-        homework1d.setText( "剩余时间不足1天的作业：" + Integer.toString( App.student.getDayLimitHomeworkCnt(1) - fix ) + " 项" ) ;
-        homeworktle.setText( "超时的作业：" + Integer.toString( App.student.getDayLimitHomeworkCnt(0) - fix ) + " 项" ) ;
+        int le0 = App.student.getDayLimitHomeworkCnt(0) - fix ;
+        int le1 = App.student.getDayLimitHomeworkCnt(1) - fix - le0 ;
+        int le3 = App.student.getDayLimitHomeworkCnt(3) - fix - le1 ;
+        int le = App.student.getDayLimitHomeworkCnt(999) - fix - le3 ;
+        homeworktot.setText( "待完成的作业：" + Integer.toString( le ) + " 项" ) ;
+        homework3d.setText( "剩余时间不足3天的作业：" + Integer.toString( le3 ) + " 项" ) ;
+        homework1d.setText( "剩余时间不足1天的作业：" + Integer.toString( le1 ) + " 项" ) ;
+        homeworktle.setText( "超时的作业：" + Integer.toString( le0 ) + " 项" ) ;
 
         String buf = "[]";
         int total = App.update.size() ;
